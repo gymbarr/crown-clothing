48371e8fe8c304933408186998d05e72a1f7c42f
Andrey Timakhovich <stewart96.at@gmail.com>
2022-12-13 10:43:51 +0300

    add routes to directories pages on the main page

--- src/components/directory-item/directory-item.jsx
+++ src/components/directory-item/directory-item.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom"
+
 import {
   BackgroundImage,
   Body,
@@ -5,9 +7,13 @@ import {
 } from "./directory-item.styles"
 
 const DirectoryItem = ({ directory }) => {
-  const { imageUrl, title } = directory
+  const { imageUrl, title, route } = directory
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route)
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
