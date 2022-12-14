31217b74701d3f1d9ab665fee537d0f1768d2a4d
Andrey Timakhovich <stewart96.at@gmail.com>
2022-12-14 16:29:28 +0300

    add redux-persist library to save cart items after refreshing the page

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { logger } from 'redux-logger'
+import { configureStore } from "@reduxjs/toolkit"
+import { logger } from "redux-logger"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
-import { rootReducer } from './root-reducer'
+import { rootReducer } from "./root-reducer"
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: [logger]
+  reducer: persistedReducer,
+  middleware: [logger],
 })
+
+export const persistor = persistStore(store)
